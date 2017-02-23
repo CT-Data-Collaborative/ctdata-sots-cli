0f82c8e11684716022d2155d6ae2910533538201
Sasha Cuerda <sasha.cuerda@gmail.com>
2017-02-23 09:48:12 -0500

    turned off echo in engine

--- ctdata_sots_cli/load/helpers.py
+++ ctdata_sots_cli/load/helpers.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 
 def setup_engine(DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME):
     PG_URI = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME)
-    engine = create_engine(PG_URI, echo=True)
+    engine = create_engine(PG_URI, echo=False)
     return engine
